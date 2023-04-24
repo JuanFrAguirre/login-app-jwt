9a8e5ce45c84ad9cf6e3951df49bfee41f7bf051
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2023-04-24 15:14:13 +0200

    updated register token verification

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import * as authController from '../controllers/auth.controller.js'
+import { verifyToken } from '../middlewares/verifyToken.js'
+import { validateAdmin } from '../middlewares/validateRoles.js'
 const router = Router()
 
 router
-  .post('/register', authController.register)
+  .post('/register', [verifyToken, validateAdmin], authController.register)
   .post('/login', authController.login)
 
 export default router
